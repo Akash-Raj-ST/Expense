4227602a6284cd252d07d09b1564731ce82f836b
07Rochak <rochakshrivastav02@gmail.com>
2022-05-19 18:31:40 +0530

    Home page and initial page development

--- Screen/Settings.js
+++ Screen/Settings.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Add() {
+export default function Settings() {
   return (
     <View>
-      <Text>Add</Text>
+      <Text>Settings</Text>
     </View>
   )
 }
