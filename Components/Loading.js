0fe5cad1d1ff4fc7abc51d89ed44f12d7250a9cf
Akash Raj ST <akashraj49070@gmail.com>
2022-06-06 12:58:41 +0530

    loading done

--- Components/Loading.js
+++ Components/Loading.js
@@ -2,26 +2,18 @@ import { View, Modal , Text} from 'react-native'
 import React from 'react'
 import {StatusBar} from 'react-native';
 
-//import LottieView from 'lottie-react-native';
-
-// export default function Loading(){
-//     return(
-//         <View>
-//             <Modal
-//                 animationType="slide"
-//                 transparent={false}
-//                 visible={true}
-//             >
-//                <LottieView source={require("../assets/animation/loading.json")} autoPlay loop/> 
-//             </Modal>
-//         </View>
-//     )
-// }
+import LottieView from 'lottie-react-native';
 
 export default function Loading(){
     return(
         <View style={{marginTop: StatusBar.currentHeight,}}>
-            <Text>Loading...</Text>
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={true}
+            >
+               <LottieView source={require("../assets/animation/loading.json")} autoPlay loop/> 
+            </Modal>
         </View>
     )
 }
